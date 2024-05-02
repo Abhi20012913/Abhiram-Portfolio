0bf53c311dea33f37680df125be7c75a1e4f0ce0
Kakarla Sai Abhiram <kakarla.saiabhiram@populardigital.ai>
2024-05-02 14:36:10 +0530

    new-builds

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Abhiram-Portfolio/'
+  base: '/Portfolio/'
 })
+
