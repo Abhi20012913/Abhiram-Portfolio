86ab68c8106a0f2474c819239369d1e48b1ecf5f
Kakarla Sai Abhiram <kakarla.saiabhiram@populardigital.ai>
2024-05-03 14:55:14 +0530

    new-builds

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from 'react';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Home from './Pages/Home/Home.jsx';
 import About from './Pages/About/About.jsx';
 import Protfolio from './Pages/Protfolio/Protfolio.jsx';
 import Contact from './Pages/Contact/Contact.jsx';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path: "/",
+    path: '/',
     element: <Home />,
   },
   {
-    path: "/About",
-    element: <About />
+    path: '/About',
+    element: <About />,
   },
   {
-    path: "/Protfolio",
-    element: <Protfolio />
+    path: '/Protfolio',
+    element: <Protfolio />,
   },
   {
-    path: "/Contact",
-    element: <Contact />
-  }
-
+    path: '/Contact',
+    element: <Contact />,
+  },
 ]);
+
 function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App;
